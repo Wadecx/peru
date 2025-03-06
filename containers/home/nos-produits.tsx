d408778e3b9f1166a58cb083ba9a696b31c359da
Wadecx <tyronmorcel@gmail.com>
2025-03-06 15:58:18 +0100

    Updates

--- containers/home/nos-produits.tsx
+++ containers/home/nos-produits.tsx
@@ -1,4 +1,5 @@
 import { products } from "@/data/products";
+import { ArrowUpRight } from "lucide-react";
 import Image from "next/image";
 
 export const Nosproduits = () => {
@@ -18,7 +19,7 @@ export const Nosproduits = () => {
       <div className="grid md:grid-cols-3 md:gap-2 gap-4 md:ml-12 ml-7 w-full">
         {products.map((carburant, index) => (
           <div
-            className="p-4 flex flex-col gap-4 max-h-[515px] shadow-[0px_0px_18px_5px_rgba(0,_0,_0,_0.1)] max-w-[90%] justify-between rounded-3xl"
+            className="p-4 flex flex-col gap-4 md:max-h-[500px] max-h-[430px] shadow-[0px_0px_18px_5px_rgba(0,_0,_0,_0.1)] max-w-[90%] justify-between rounded-3xl"
             key={index}
           >
             <div className="flex gap-2 items-center mt-4">
@@ -35,7 +36,11 @@ export const Nosproduits = () => {
               height={300}
               className=""
             ></Image>
-
+            <div className="relative right-10 bottom-20 justify-end flex">
+              <button className="p-2 bg-secondary relative md:left-0 left-8 md:top-0 top-3 rounded-full text-white">
+                <ArrowUpRight size={64} className="md:w-full h-full w-[30px] h-[30px]" />
+              </button>
+            </div>
           </div>
         ))}
       </div>
